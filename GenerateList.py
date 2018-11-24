8220895129c13dc10cdd823ccff7f50ba86b753b
Atmadeep Banerjee <atmadeepb@gmail.com>
2018-11-24 22:56:03 +0530

    Delete GenerateList.py

--- GenerateList.py
+++
@@ -1,5 +0,0 @@
-from DuckDuckImageScrape import search
-
-with open('Safe/PokeList (copy).txt', 'r') as f:
-    for pokemon in f:
-        search(pokemon, max_fetch=8, out='PokeImageFile.txt', png = False)
